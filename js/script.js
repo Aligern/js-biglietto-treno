dc3ef39b00cb83debf06a92a28a7ed5e9e62c6c8
Aligern <sasso.luca93@gmail.com>
2024-03-07 10:11:28 +0100

    Comments

--- js/script.js
+++ js/script.js
@@ -7,27 +7,34 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 
 
 // chiediamo quanti Km vuole percorrere l'utente
-let meters = (prompt ('Inserisci Kilometraggio'));
+// utilizziamo parseInt per effettuare un controllo sulle informazioni inserite dall'utente
+let meters = parseInt(prompt ('Inserisci Kilometraggio'));
 console.log(meters);
 // chiediamo l'eta all'utente
-let age = (prompt ('Inserisci età'));
+let age = parseInt(prompt ('Inserisci età'));
 console.log(age);
 //prezzo del biglietto per km
-let price = 0.21 * meters;
+let price = meters * 0.21;
 //sconto minorenni
 let under18 = 20 / 100;
 //sconto anziani
 let over65 = 40 / 100;
 
+
+
 if (isNaN(age) || isNaN(meters)) {
    document.getElementById('display').innerHTML = 'Inserisci dei numeri';
+   console.log(age)
+   console.log(meters)
 } else {
    if (age < 18) {
-      price = price - (price * under18);
+      // abbreviamo l'operazione utilizzando * -= * per evitare di scrivere nuovamente * price - *
+      price -= (price * under18);
       console.log(price);
    } else if (age >= 65) { 
-      price = price - (price * over65);
+      price -= (price * over65);
       console.log(price);
    } 
-   document.getElementById('display').innerHTML =` il prezzo è: ${price} `;
+   document.getElementById('display').innerHTML =` il prezzo è: ${price.toFixed(2)} `;
+   // utilizziamo toFixed per effettuare una formattazione e convertire un numero in una stringa ( questo preclude l'utilizzo del numero per usi futuri )
 }
